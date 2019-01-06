12d3623754aa79b8f5bd9ee0392cb0c15c9c7039
Бауыржан <@->
2019-01-07 00:35:06 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿//Установил Visual Studio 2015
+//Создал простое консольное приложение и ознокомлен с его структурой
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homework");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//Прочитал материалы
